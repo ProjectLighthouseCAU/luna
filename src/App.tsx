8b2dc644d0d2244503e45d5a10c89925d7acb64c
fwcd <fwcdmail@gmail.com>
2022-11-29 18:32:16 +0100

    Set up basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import { Display } from './components/Display';
 
-function App() {
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Display />
+    </Container>
   );
 }
-
-export default App;
---
+++ src/components/Display/Display.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function Display() {
+  return <div>Display coming soon...</div>;
+}
