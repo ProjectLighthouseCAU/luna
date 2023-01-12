2852b2036b740908c612b2947e8fddafd839206b
fwcd <fwcdmail@gmail.com>
2023-01-12 03:59:02 +0100

    Add next-themes for managing light/dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import { AppContainer } from '@luna/AppContainer';
 import { WindowDimensionsContextProvider } from '@luna/contexts/WindowDimensions';
-import { NextUIProvider } from '@nextui-org/react';
+import { createTheme, NextUIProvider } from '@nextui-org/react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import React from 'react';
+
+const lightTheme = createTheme({
+  type: 'light',
+});
+
+const darkTheme = createTheme({
+  type: 'dark',
+});
 
 export function App() {
   return (
-    <NextUIProvider>
-      <WindowDimensionsContextProvider>
-        <AppContainer />
-      </WindowDimensionsContextProvider>
-    </NextUIProvider>
+    <NextThemesProvider
+      defaultTheme="system"
+      attribute="class"
+      value={{ light: lightTheme.className, dark: darkTheme.className }}
+    >
+      <NextUIProvider>
+        <WindowDimensionsContextProvider>
+          <AppContainer />
+        </WindowDimensionsContextProvider>
+      </NextUIProvider>
+    </NextThemesProvider>
   );
 }
