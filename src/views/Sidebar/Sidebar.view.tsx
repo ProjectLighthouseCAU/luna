7d6a04232932957956d4af2bcf4b20e6d948bdd8
fwcd <fwcdmail@gmail.com>
2023-01-17 00:07:55 +0100

    Style links

--- src/views/Sidebar/Sidebar.view.tsx
+++ src/views/Sidebar/Sidebar.view.tsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import { AuthContext } from '@luna/contexts/Auth';
+import { Divider } from '@nextui-org/react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 export function Sidebar() {
+  const auth = useContext(AuthContext);
+
   return (
-    <ul>
-      <li>
-        <Link to="admin">Admin</Link>
-      </li>
-      <li>
-        <Link to="displays">Displays</Link>
-      </li>
-    </ul>
+    <>
+      <ul>
+        <li>
+          <Link to="admin">Admin</Link>
+        </li>
+        <li>
+          <Link to="displays">Displays</Link>
+        </li>
+      </ul>
+      <Divider />
+      <ul>
+        <li>
+          <Link
+            onClick={() => {
+              auth.setToken();
+            }}
+            to="/login"
+            className="signout"
+          >
+            Sign out
+          </Link>
+        </li>
+      </ul>
+    </>
   );
 }
