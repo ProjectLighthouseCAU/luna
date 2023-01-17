b9e11db0d7ef182f3a96f3ad8739d1976f052df3
fwcd <fwcdmail@gmail.com>
2023-01-17 15:40:33 +0100

    Add compact/mobile home layout

--- src/screens/Home/Home.screen.tsx
+++ src/screens/Home/Home.screen.tsx
@@ -1,19 +1,32 @@
+import { Breakpoint, useBreakpoint } from '@luna/hooks/Breakpoint';
 import { useRouteNode } from '@luna/hooks/RouteNode';
 import { Sidebar } from '@luna/views/Sidebar/Sidebar.view';
-import { Grid, Text } from '@nextui-org/react';
+import { Button, Container, Grid, Row, Text } from '@nextui-org/react';
+import { IconMenu2 } from '@tabler/icons';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 export function HomeScreen() {
   const routeNode = useRouteNode();
+  const breakpoint = useBreakpoint();
+  const compact = breakpoint <= Breakpoint.Sm;
 
   return (
     <Grid.Container gap={4}>
+      {!compact ? (
+        <Grid>
+          <Sidebar />
+        </Grid>
+      ) : null}
       <Grid>
-        <Sidebar />
-      </Grid>
-      <Grid>
-        <Text h2>{routeNode?.displayName}</Text>
+        <Grid.Container alignItems="baseline">
+          {compact ? (
+            <Button light auto>
+              <IconMenu2 />
+            </Button>
+          ) : null}
+          <Text h2>{routeNode?.displayName}</Text>
+        </Grid.Container>
         <Outlet />
       </Grid>
     </Grid.Container>
