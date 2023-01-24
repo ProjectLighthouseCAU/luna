a5564b965d91b594b0f0a1417db02accdf8f5920
fwcd <fwcdmail@gmail.com>
2023-01-24 17:54:01 +0100

    Remove redundant extra <Container>

--- src/AppContainer.tsx
+++ src/AppContainer.tsx
@@ -1,6 +1,5 @@
 import React, { useContext } from 'react';
 import { LoginScreen } from '@luna/screens/Login';
-import { Container } from '@nextui-org/react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { HomeScreen } from '@luna/screens/Home';
 import { NotFoundScreen } from '@luna/screens/NotFound';
@@ -35,20 +34,16 @@ export function AppContainer() {
   const auth = useContext(AuthContext);
 
   return (
-    <Container>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            auth.token ? <HomeScreen /> : <Navigate replace to="login" />
-          }
-        >
-          <Route index element={<Navigate replace to="displays" />} />
-          {ROUTE_TREE.children?.map(node => routerRoute({ node }))}
-        </Route>
-        <Route path="/login" element={<LoginScreen />} />
-        <Route path="*" element={<NotFoundScreen />} />
-      </Routes>
-    </Container>
+    <Routes>
+      <Route
+        path="/"
+        element={auth.token ? <HomeScreen /> : <Navigate replace to="login" />}
+      >
+        <Route index element={<Navigate replace to="displays" />} />
+        {ROUTE_TREE.children?.map(node => routerRoute({ node }))}
+      </Route>
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="*" element={<NotFoundScreen />} />
+    </Routes>
   );
 }
