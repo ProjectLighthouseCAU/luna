a2ec1b18406a7c43d642897efd9235e84f940f6a
fwcd <fwcdmail@gmail.com>
2023-08-15 19:40:48 +0200

    Install tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
 import { AppContainer } from '@luna/AppContainer';
 import { AuthContextProvider } from '@luna/contexts/AuthContext';
 import { WindowDimensionsContextProvider } from '@luna/contexts/WindowDimensionsContext';
-import { createTheme, NextUIProvider } from '@nextui-org/react';
+import { NextUIProvider } from '@nextui-org/react';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-const darkTheme = createTheme({
-  type: 'dark',
-  theme: {
-    colors: {
-      primaryBorder: '#FF0000',
-      backgroundContrast: '#151515', // e.g. card backgrounds
-      accents0: '#252525', // e.g. input backgrounds
-      selection: '#3C009C',
-    },
-  },
-});
-
 export function App() {
   return (
-    <NextUIProvider theme={darkTheme}>
+    <NextUIProvider>
       <WindowDimensionsContextProvider>
         <AuthContextProvider>
           <BrowserRouter>
