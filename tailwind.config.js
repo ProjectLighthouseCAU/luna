47ad0ea63d7524fe32013a904032076bfc03c7d7
fwcd <fwcdmail@gmail.com>
2023-08-16 03:54:10 +0200

    Configure Tailwind for source files
    Otherwise Tailwind classes would not work correctly in our source files.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ const { nextui } = require('@nextui-org/react');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+    './src/**/*.{js,jsx,ts,tsx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
