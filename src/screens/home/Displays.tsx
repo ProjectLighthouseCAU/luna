3896d406fe6902a55fd70a9210b85975cd619c6c
fwcd <fwcdmail@gmail.com>
2023-08-18 00:31:46 +0200

    Lay out displays with `flex-wrap` (for now)

--- src/screens/home/Displays.tsx
+++ src/screens/home/Displays.tsx
@@ -3,14 +3,15 @@ import { Pagination } from '@nextui-org/react';
 import React from 'react';
 
 export function Displays() {
-  // TODO: Responsive/grid layout
   return (
-    <div>
-      {[...new Array(10)].map((_, i) => (
-        <div key={`${i}`}>
-          <DisplayCard username={`Test ${i}`} />
-        </div>
-      ))}
+    <div className="flex flex-col space-y-4">
+      <div className="flex flex-wrap">
+        {[...new Array(10)].map((_, i) => (
+          <div key={`${i}`}>
+            <DisplayCard username={`Test ${i}`} />
+          </div>
+        ))}
+      </div>
       <Pagination total={20} initialPage={1} />
     </div>
   );
