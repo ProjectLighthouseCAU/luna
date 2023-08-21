e631dbdb94c090d28ade18d326a336248a04f3b1
fwcd <fwcdmail@gmail.com>
2023-08-21 20:56:57 +0200

    Add `useAsyncIterable` hook

---
+++ src/hooks/useAsyncIterable.ts
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+
+// https://stackoverflow.com/a/68791731
+
+export function useAsyncIterable<T>(
+  iterable: () => AsyncIterable<T>,
+  consumer: (value: T) => Promise<void>
+) {
+  useEffect(() => {
+    let isCancelled = false;
+    (async () => {
+      for await (const value of iterable()) {
+        consumer(value);
+        if (!isCancelled) {
+          break;
+        }
+      }
+    })();
+    return () => {
+      isCancelled = true;
+    };
+  }, [iterable, consumer]);
+}
