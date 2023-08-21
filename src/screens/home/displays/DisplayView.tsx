ac4bff91fdc77647992df1f2828eee3a1730ff69
fwcd <fwcdmail@gmail.com>
2023-08-21 22:02:02 +0200

    Handle missing model case in `DisplayView`

--- src/screens/home/displays/DisplayView.tsx
+++ src/screens/home/displays/DisplayView.tsx
@@ -5,16 +5,19 @@ import { useContext } from 'react';
 import { useParams } from 'react-router-dom';
 
 export function DisplayView() {
-  const { username } = useParams();
+  const { username } = useParams() as { username: string };
   const { userModels } = useContext(ModelContext);
+
+  const userModel = userModels.get(username);
 
   return (
     <HomeContent title={`${username}'s Display`}>
-      {/* TODO: Handle errors */}
-      <DisplayCard
-        username={username!}
-        frame={userModels.get(username!)!.frame}
-      />
+      {userModel ? (
+        <DisplayCard username={username} frame={userModel.frame} />
+      ) : (
+        // TODO: Improve error message, perhaps add a link back to /displays?
+        <p>No model found!</p>
+      )}
     </HomeContent>
   );
 }
