1683a738842413f0651f8794673674f9132cd1de
fwcd <fwcdmail@gmail.com>
2023-08-22 14:03:28 +0200

    Stub out options card

--- src/screens/home/displays/DisplayInspector.tsx
+++ src/screens/home/displays/DisplayInspector.tsx
@@ -1,10 +1,12 @@
 import { DisplayInspectorApiTokenCard } from '@luna/screens/home/displays/DisplayInspectorApiTokenCard';
 import { DisplayInspectorInputCard } from '@luna/screens/home/displays/DisplayInspectorInputCard';
+import { DisplayInspectorOptionsCard } from '@luna/screens/home/displays/DisplayInspectorOptionsCard';
 
 export function DisplayInspector() {
   // TODO: Implement actual functionality
   return (
     <div className="flex flex-col space-y-3">
+      <DisplayInspectorOptionsCard />
       <DisplayInspectorApiTokenCard />
       <DisplayInspectorInputCard />
     </div>
---
+++ src/screens/home/displays/DisplayInspectorOptionsCard.tsx
@@ -0,0 +1,11 @@
+import { DisplayInspectorCard } from '@luna/screens/home/displays/DisplayInspectorCard';
+import { IconAdjustments } from '@tabler/icons-react';
+
+export function DisplayInspectorOptionsCard() {
+  return (
+    <DisplayInspectorCard
+      icon={<IconAdjustments />}
+      title="Options"
+    ></DisplayInspectorCard>
+  );
+}
