4f32d58d900268b296dd95b42e5928ba60205f02
fwcd <fwcdmail@gmail.com>
2023-08-22 02:39:36 +0200

    Add API token modal

--- src/screens/home/displays/DisplayInspectorApiTokenCard.tsx
+++ src/screens/home/displays/DisplayInspectorApiTokenCard.tsx
@@ -1,14 +1,46 @@
 import { DisplayInspectorCard } from '@luna/screens/home/displays/DisplayInspectorCard';
-import { Button, Tooltip } from '@nextui-org/react';
+import {
+  Button,
+  Code,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+  Tooltip,
+  useDisclosure,
+} from '@nextui-org/react';
 import { IconClipboard, IconLink } from '@tabler/icons-react';
 
 export function DisplayInspectorApiTokenCard() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  // TODO: Use actual API token
+
   return (
     <DisplayInspectorCard icon={<IconLink />} title="API Token">
       <div className="flex flex-row items-center space-x-1">
         <Tooltip content="Show the token">
-          <Button size="md">Reveal</Button>
+          <Button size="md" onPress={onOpen}>
+            Reveal
+          </Button>
         </Tooltip>
+        <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+          <ModalContent>
+            {onClose => (
+              <>
+                <ModalHeader>API Token</ModalHeader>
+                <ModalBody className="p-4">
+                  <p>Your token is valid through DD.MM.2023.</p>
+                  <Code>API-Tok-ABC</Code>
+                  <Button>
+                    <IconClipboard />
+                    Copy Token
+                  </Button>
+                </ModalBody>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
         <Tooltip content="Copy the token">
           <Button isIconOnly size="md">
             <IconClipboard />
