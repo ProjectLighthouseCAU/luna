0b8bbfdf7101e182cd8bebda2d3386c99478877a
fwcd <fwcdmail@gmail.com>
2023-08-22 03:35:17 +0200

    Implement legacy auth for now

--- src/client/auth/LegacyAuthClient.ts
+++ src/client/auth/LegacyAuthClient.ts
@@ -4,16 +4,45 @@ import { Token } from '@luna/client/auth/Token';
 // FIXME: Implement this client
 
 export class LegacyAuthClient implements AuthClient {
+  // TODO: Make this customizable
+  private username?: string;
+
+  constructor(
+    private readonly url: string = 'https://lighthouse.uni-kiel.de'
+  ) {}
+
   async logIn(username: string, password: string): Promise<boolean> {
-    return false;
+    this.username = username;
+
+    const body = new URLSearchParams();
+    body.append('username', username);
+    body.append('password', password);
+
+    const response = await fetch(`${this.url}/login`, {
+      method: 'POST',
+      mode: 'cors',
+      credentials: 'include',
+      body,
+    });
+
+    if (response.status !== 303) {
+      console.warn(
+        `Legacy backend responded with ${response.status} to login request`
+      );
+      return false;
+    }
+
+    return true;
   }
 
   async logOut(): Promise<boolean> {
-    return false;
+    this.username = undefined;
+    return true;
   }
 
   async getPublicUsers(): Promise<string[]> {
-    return [];
+    // TODO: Fetch all public users
+    return this.username !== undefined ? [this.username] : [];
   }
 
   async getToken(): Promise<Token | null> {
