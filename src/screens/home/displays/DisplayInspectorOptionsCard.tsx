38843eae11ce819464756ee5b606f0d118b5c925
fwcd <fwcdmail@gmail.com>
2023-08-22 14:11:16 +0200

    Add public switch

--- src/screens/home/displays/DisplayInspectorOptionsCard.tsx
+++ src/screens/home/displays/DisplayInspectorOptionsCard.tsx
@@ -1,11 +1,11 @@
 import { DisplayInspectorCard } from '@luna/screens/home/displays/DisplayInspectorCard';
+import { Switch } from '@nextui-org/react';
 import { IconAdjustments } from '@tabler/icons-react';
 
 export function DisplayInspectorOptionsCard() {
   return (
-    <DisplayInspectorCard
-      icon={<IconAdjustments />}
-      title="Options"
-    ></DisplayInspectorCard>
+    <DisplayInspectorCard icon={<IconAdjustments />} title="Options">
+      <Switch>Public</Switch>
+    </DisplayInspectorCard>
   );
 }
