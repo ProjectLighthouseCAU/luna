eb50946d1897a42da45637b7480de3dbae0e9a6e
fwcd <fwcdmail@gmail.com>
2023-08-22 02:27:01 +0200

    Add API token card

--- src/screens/home/displays/DisplayInspector.tsx
+++ src/screens/home/displays/DisplayInspector.tsx
@@ -1,12 +1,27 @@
 import { DisplayInspectorCard } from '@luna/screens/home/displays/DisplayInspectorCard';
-import { Switch } from '@nextui-org/react';
-import { IconDeviceGamepad2, IconLink } from '@tabler/icons-react';
+import { Button, Switch, Tooltip } from '@nextui-org/react';
+import {
+  IconClipboard,
+  IconDeviceGamepad2,
+  IconLink,
+} from '@tabler/icons-react';
 
 export function DisplayInspector() {
   // TODO: Implement actual functionality
   return (
     <div className="flex flex-col space-y-3">
-      <DisplayInspectorCard icon={<IconLink />} title="API Token" />
+      <DisplayInspectorCard icon={<IconLink />} title="API Token">
+        <div className="flex flex-row items-center space-x-1">
+          <Tooltip content="Show the token">
+            <Button size="md">Reveal</Button>
+          </Tooltip>
+          <Tooltip content="Copy the token">
+            <Button isIconOnly size="md">
+              <IconClipboard />
+            </Button>
+          </Tooltip>
+        </div>
+      </DisplayInspectorCard>
       <DisplayInspectorCard icon={<IconDeviceGamepad2 />} title="Input">
         <div className="flex flex-col space-y-2">
           <Switch>Keyboard</Switch>
