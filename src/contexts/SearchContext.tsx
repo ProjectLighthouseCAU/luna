b3ae527c24a4ede3e4ab7fee43fe92de144d9809
fwcd <fwcdmail@gmail.com>
2023-08-29 14:20:42 +0200

    Add `SearchContext`

---
+++ src/contexts/SearchContext.tsx
@@ -0,0 +1,21 @@
+import { ReactNode, createContext } from 'react';
+
+export interface Search {
+  query: string;
+}
+
+export const SearchContext = createContext<Search>({
+  query: '',
+});
+
+interface SearchContextProviderProps {
+  query: string;
+  children: ReactNode;
+}
+
+export function SearchContextProvider({
+  query,
+  children,
+}: SearchContextProviderProps) {
+  <SearchContext.Provider value={{ query }}>{children}</SearchContext.Provider>;
+}
