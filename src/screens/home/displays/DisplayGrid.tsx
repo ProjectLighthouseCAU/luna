1287c3fb2805e35f94c7a5492c11776aee00a3a5
fwcd <fwcd@fwcd.dev>
2024-03-14 20:56:07 +0100

    Factor out DisplayGrid

---
+++ src/screens/home/displays/DisplayGrid.tsx
@@ -0,0 +1,42 @@
+import { UserModel } from '@luna/client/model/UserModel';
+import { DisplayCard } from '@luna/components/DisplayCard';
+import { InView } from 'react-intersection-observer';
+import { Link } from 'react-router-dom';
+
+export interface DisplayGridProps {
+  userModels: Immutable.Map<string, UserModel>;
+  searchQuery: string;
+  displayWidth: number;
+}
+
+export function DisplayGrid({
+  userModels,
+  searchQuery,
+  displayWidth,
+}: DisplayGridProps) {
+  return (
+    <div className="flex flex-wrap gap-4 justify-center">
+      {[...userModels.entries()]
+        .filter(([username]) =>
+          username.toLowerCase().includes(searchQuery.toLowerCase())
+        )
+        .sort(([u1], [u2]) => u1.localeCompare(u2))
+        .map(([username, userModel]) => (
+          <Link to={username} key={username}>
+            <InView>
+              {({ inView, ref }) => (
+                <div ref={ref}>
+                  <DisplayCard
+                    username={username}
+                    frame={userModel.frame}
+                    displayWidth={displayWidth}
+                    isSkeleton={!inView}
+                  />
+                </div>
+              )}
+            </InView>
+          </Link>
+        ))}
+    </div>
+  );
+}
--- src/screens/home/displays/DisplaysView.tsx
+++ src/screens/home/displays/DisplaysView.tsx
@@ -1,13 +1,11 @@
-import { DisplayCard } from '@luna/components/DisplayCard';
 import { LocalStorageKey } from '@luna/constants/LocalStorageKey';
 import { ModelContext } from '@luna/contexts/ModelContext';
 import { SearchContext } from '@luna/contexts/SearchContext';
 import { useLocalStorage } from '@luna/hooks/useLocalStorage';
 import { HomeContent } from '@luna/screens/home/HomeContent';
+import { DisplayGrid } from '@luna/screens/home/displays/DisplayGrid';
 import { DisplaysToolbar } from '@luna/screens/home/displays/DisplaysToolbar';
 import { useContext } from 'react';
-import { InView } from 'react-intersection-observer';
-import { Link } from 'react-router-dom';
 
 export function DisplaysView() {
   const { query } = useContext(SearchContext);
@@ -32,30 +30,12 @@ export function DisplaysView() {
         />
       }
     >
-      <div className="flex flex-col space-y-4">
-        <div className="flex flex-wrap gap-4 justify-center">
-          {[...userModels.entries()]
-            .filter(([username]) =>
-              username.toLowerCase().includes(query.toLowerCase())
-            )
-            .sort(([u1], [u2]) => u1.localeCompare(u2))
-            .map(([username, userModel]) => (
-              <Link to={username} key={username}>
-                <InView>
-                  {({ inView, ref }) => (
-                    <div ref={ref}>
-                      <DisplayCard
-                        username={username}
-                        frame={userModel.frame}
-                        displayWidth={displayWidth}
-                        isSkeleton={!inView}
-                      />
-                    </div>
-                  )}
-                </InView>
-              </Link>
-            ))}
-        </div>
+      <div className="flex flex-col">
+        <DisplayGrid
+          userModels={userModels}
+          searchQuery={query}
+          displayWidth={displayWidth}
+        />
       </div>
     </HomeContent>
   );
