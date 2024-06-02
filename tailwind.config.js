f3afb706a06c78bafe1fea714c665fcf924550d8
fwcd <fwcd@fwcd.dev>
2024-06-02 02:40:25 +0200

    Resolve node_modules dynamically in Tailwind config
    This fixes an issue where the files could not be located if node_modules
    is in a parent directory, as e.g. is the case if luna is embedded as
    part of an npm workspace.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 const { nextui } = require('@nextui-org/react');
+const path = require('path');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    path.join(
+      // Use `require.resolve` to locate `node_modules/@nextui-org/theme`
+      // dynamically (node_modules might be in a parent directory if luna is
+      // part of an npm workspace, as e.g. in luna-desktop)
+      path.dirname(require.resolve('@nextui-org/theme/package.json')),
+      'dist/**/*.{js,ts,jsx,tsx}'
+    ),
   ],
   theme: {
     extend: {},
