95a3e9492b4083c6bd1a5c16906cdf4295538d71
fwcd <fwcd@fwcd.dev>
2024-08-26 02:35:45 +0200

    Implement experimental login for new auth API

--- src/api/auth/LighthouseAuthApi.ts
+++ src/api/auth/LighthouseAuthApi.ts
@@ -2,7 +2,30 @@ import { AuthApi } from '@luna/api/auth/AuthApi';
 import { Token } from '@luna/api/auth/Token';
 import { User } from '@luna/api/auth/User';
 
+// TODO: Auto-generate these types from the Swagger definition?
+
+interface ApiUser {
+  created_at: string;
+  email: string;
+  id: number;
+  last_login: string;
+  permanent_api_token: boolean;
+  registration_key?: {
+    created_at: string;
+    description: string;
+    expires_at: string;
+    id: number;
+    key: string;
+    permanent: boolean;
+    updated_at: string;
+  };
+  updated_at: string;
+  username: string;
+}
+
 export class LighthouseAuthApi implements AuthApi {
+  private user?: User;
+
   constructor(
     private readonly url: string = 'https://lighthouse.uni-kiel.de/api'
   ) {}
@@ -16,11 +39,45 @@ export class LighthouseAuthApi implements AuthApi {
   }
 
   async logIn(username?: string, password?: string): Promise<User | null> {
-    throw new Error('Method not implemented.');
+    if (!username || !password) {
+      return null;
+    }
+
+    await fetch(`${this.url}/login`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });
+
+    const apiUserResponse = await fetch(
+      `${this.url}/users?${new URLSearchParams({
+        name: username,
+      })}`,
+      {
+        credentials: 'include',
+      }
+    );
+    const apiUser: ApiUser = await apiUserResponse.json();
+    const user: User = {
+      username: apiUser.username,
+    };
+
+    return user;
   }
 
   async logOut(): Promise<boolean> {
-    throw new Error('Method not implemented.');
+    await fetch(`${this.url}/logout`, {
+      method: 'POST',
+      credentials: 'include',
+    });
+
+    return true;
   }
 
   async getPublicUsers(): Promise<User[]> {
