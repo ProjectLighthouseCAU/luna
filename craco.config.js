5259e013eb1e0b4d6a497a1d40c5024fd6b882f9
fwcd <fwcd@fwcd.dev>
2024-09-26 06:05:55 +0200

    Ignore ResizeObserver messages for now

--- craco.config.js
+++ craco.config.js
@@ -3,6 +3,22 @@ const process = require('process');
 
 module.exports = {
   devServer: {
+    client: {
+      overlay: {
+        runtimeErrors: error => {
+          // NOTE: We forward ResizeObserver errors to the console, these seem
+          // to come from framer-motion during the card flip on the login
+          // screen. According to https://stackoverflow.com/a/77591424 we
+          // shouldn't worry about this in our production build, so we'll just
+          // adopt the workaround from https://stackoverflow.com/a/77914968 and
+          // ignore these errors in dev builds.
+          if (error?.message.startsWith('ResizeObserver')) {
+            return false;
+          }
+          return true;
+        },
+      },
+    },
     port: parseInt(process.env['PORT'] ?? '4000'),
   },
   webpack: {
