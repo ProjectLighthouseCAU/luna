e361837a66a7f63b2708c7c57ca54b3960564add
fwcd <fwcd@fwcd.dev>
2024-09-26 03:26:00 +0200

    Animate the login/signup card

--- src/screens/login/LoginScreen.tsx
+++ src/screens/login/LoginScreen.tsx
@@ -6,6 +6,7 @@ import { SignupCard } from '@luna/screens/login/SignupCard';
 import { useContext, useState } from 'react';
 import { DISPLAY_ASPECT_RATIO } from '@luna/components/Display';
 import { Logo } from '@luna/components/Logo';
+import { motion } from 'framer-motion';
 
 export function LoginScreen() {
   const { width, height } = useContext(WindowDimensionsContext);
@@ -29,14 +30,18 @@ export function LoginScreen() {
         isHorizontal ? 'flex-row justify-center' : 'flex-col mt-4'
       } items-center ${isHorizontal ? 'space-x-6' : 'space-y-6'}`}
     >
-      <div className="flex flex-col space-y-8 min-w-80 items-center">
+      <div className="flex flex-col space-y-8 items-center">
         {!showSignup && isHorizontal ? <Logo /> : null}
-        <h1 className="text-4xl font-bold">Project Lighthouse</h1>
-        {showSignup ? (
-          <SignupCard showLogin={() => setShowSignup(false)} />
-        ) : (
-          <LoginCard showSignup={() => setShowSignup(true)} />
-        )}
+        <motion.h1 layout className="text-4xl font-bold">
+          Project Lighthouse
+        </motion.h1>
+        <motion.div layout className="min-w-80">
+          {showSignup ? (
+            <SignupCard showLogin={() => setShowSignup(false)} />
+          ) : (
+            <LoginCard showSignup={() => setShowSignup(true)} />
+          )}
+        </motion.div>
       </div>
       <LiveDisplay width={displayWidth} />
     </div>
