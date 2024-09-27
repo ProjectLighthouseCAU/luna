6493e6f7eac371be1a5432b000aa7a3276dd640f
fwcd <fwcd@fwcd.dev>
2024-09-27 05:24:29 +0200

    Disable animations in grid if there are too many displays
    For performance reasons

--- src/screens/home/displays/DisplayGrid.tsx
+++ src/screens/home/displays/DisplayGrid.tsx
@@ -17,29 +17,42 @@ export function DisplayGrid({
   searchQuery,
   displayWidth,
 }: DisplayGridProps) {
+  // Filter the models case-insensitively by the search query
+  const filteredModels = [...userModels.entries()]
+    .filter(([username]) =>
+      username.toLowerCase().includes(searchQuery.toLowerCase())
+    )
+    .sort(([u1], [u2]) => u1.localeCompare(u2));
+
+  // Disable animations automatically if there are too many displays for
+  // performance reasons. Unfortunately we can't seem to change the layoutId
+  // after the component has mounted, so even if a user filters down the view
+  // the displays might not animate: https://github.com/framer/motion/issues/2075
+  const animationsEnabled = filteredModels.length <= 512;
+
   return (
     <div className="flex flex-wrap gap-4 justify-center">
-      {[...userModels.entries()]
-        .filter(([username]) =>
-          username.toLowerCase().includes(searchQuery.toLowerCase())
-        )
-        .sort(([u1], [u2]) => u1.localeCompare(u2))
-        .map(([username, userModel]) => (
-          <Link to={username} key={username}>
-            <InView>
-              {({ inView, ref }) => (
-                <motion.div ref={ref} layoutId={displayLayoutId(username)}>
-                  <DisplayCard
-                    username={username}
-                    frame={userModel.frame}
-                    displayWidth={displayWidth}
-                    isSkeleton={!inView}
-                  />
-                </motion.div>
-              )}
-            </InView>
-          </Link>
-        ))}
+      {filteredModels.map(([username, userModel]) => (
+        <Link to={username} key={username}>
+          <InView>
+            {({ inView, ref }) => (
+              <motion.div
+                ref={ref}
+                {...(animationsEnabled
+                  ? { layoutId: displayLayoutId(username) }
+                  : {})}
+              >
+                <DisplayCard
+                  username={username}
+                  frame={userModel.frame}
+                  displayWidth={displayWidth}
+                  isSkeleton={!inView}
+                />
+              </motion.div>
+            )}
+          </InView>
+        </Link>
+      ))}
     </div>
   );
 }
