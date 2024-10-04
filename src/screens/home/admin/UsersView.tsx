b2e82af2da5f974ad22a59bbab2f3f485871d701
Nico Biernat <stu209212@mail.uni-kiel.de>
2024-10-04 16:17:35 +0200

    Add icons to actions and add-user button, fix initial sorting, remove roles and registration key from table

--- src/screens/home/admin/UsersView.tsx
+++ src/screens/home/admin/UsersView.tsx
@@ -1,7 +1,9 @@
+import { User } from '@luna/api/auth/types';
 import { AuthContext } from '@luna/contexts/AuthContext';
 import { HomeContent } from '@luna/screens/home/HomeContent';
 import { getOrThrow } from '@luna/utils/result';
 import {
+  Button,
   Chip,
   Table,
   TableBody,
@@ -12,40 +14,61 @@ import {
   Tooltip,
 } from '@nextui-org/react';
 import { useAsyncList } from '@react-stately/data';
+import {
+  IconEye,
+  IconPencil,
+  IconTrash,
+  IconUserPlus,
+} from '@tabler/icons-react';
 import { useContext } from 'react';
+import { SortDescriptor } from 'react-stately';
 
 export function UsersView() {
   const auth = useContext(AuthContext);
 
+  const sortList = (items: User[], sortDescriptor: SortDescriptor): User[] => {
+    let col = sortDescriptor.column ?? 'id';
+    return items.sort((a: any, b: any): number => {
+      let first = a[col];
+      let second = b[col];
+      let cmp =
+        (parseInt(first) || first) < (parseInt(second) || second) ? -1 : 1;
+      if (sortDescriptor.direction === 'descending') {
+        cmp *= -1;
+      }
+      return cmp;
+    });
+  };
+
   const users = useAsyncList({
+    initialSortDescriptor: {
+      column: 'id',
+      direction: 'ascending',
+    },
     async load() {
       try {
-        return { items: getOrThrow(await auth.getAllUsers()) };
+        let items = getOrThrow(await auth.getAllUsers());
+        items = sortList(items, { column: 'id', direction: 'ascending' });
+        return { items };
       } catch (error) {
         console.error(`Could not fetch users for users view: ${error}`);
         return { items: [] };
       }
     },
     async sort({ items, sortDescriptor }) {
-      let col = sortDescriptor.column ?? 'id';
-      return {
-        items: items.sort((a: any, b: any): number => {
-          let first = a[col];
-          let second = b[col];
-          let cmp =
-            (parseInt(first) || first) < (parseInt(second) || second) ? -1 : 1;
-          if (sortDescriptor.direction === 'descending') {
-            cmp *= -1;
-          }
-          return cmp;
-        }),
-      };
+      return { items: sortList(items, sortDescriptor) };
     },
   });
 
   return (
     // TODO: Lazy rendering
     <HomeContent title="Users">
+      <Tooltip content="Add user" color="success">
+        <Button className="mb-5">
+          {/* TODO: add user (maybe open modal to input data) */}
+          <IconUserPlus className="text-lg text-success cursor-pointer active:opacity-50"></IconUserPlus>
+        </Button>
+      </Tooltip>
       <Table
         aria-label="Table of users for administrators"
         removeWrapper
@@ -64,9 +87,9 @@ export function UsersView() {
           <TableColumn key="email" allowsSorting>
             E-Mail
           </TableColumn>
-          <TableColumn key="roles" allowsSorting>
+          {/* TODO: move to details modal: <TableColumn key="roles" allowsSorting>
             Roles
-          </TableColumn>
+          </TableColumn> */}
           <TableColumn key="createdAt" allowsSorting>
             Created At
           </TableColumn>
@@ -79,9 +102,9 @@ export function UsersView() {
           <TableColumn key="permanentApiToken" allowsSorting>
             Permanent API-Token
           </TableColumn>
-          <TableColumn key="registrationKey" allowsSorting>
+          {/* TODO: move to details modal: <TableColumn key="registrationKey" allowsSorting>
             Registration-Key
-          </TableColumn>
+          </TableColumn> */}
           <TableColumn key="actions">Actions</TableColumn>
         </TableHeader>
         <TableBody items={users.items}>
@@ -90,7 +113,7 @@ export function UsersView() {
               <TableCell>{user.id}</TableCell>
               <TableCell>{user.username}</TableCell>
               <TableCell>{user.email}</TableCell>
-              <TableCell>{user.roles?.map(role => role.name)}</TableCell>
+              {/* TODO: move to details modal: <TableCell>{user.roles?.map(role => role.name)}</TableCell> */}
               <TableCell>{user.createdAt?.toLocaleString()}</TableCell>
               <TableCell>{user.updatedAt?.toLocaleString()}</TableCell>
               <TableCell>{user.lastSeen?.toLocaleString()}</TableCell>
@@ -105,24 +128,20 @@ export function UsersView() {
                   </Chip>
                 )}
               </TableCell>
-              <TableCell>{user.registrationKey?.key}</TableCell>
+              {/* TODO: move to details modal: <TableCell>{user.registrationKey?.key}</TableCell> */}
               <TableCell>
-                {/* TODO: use icons */}
                 <div className="relative flex items-center gap-2">
                   <Tooltip content="Details">
-                    <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                      view
-                    </span>
+                    <IconEye className="text-lg cursor-pointer active:opacity-50"></IconEye>
+                    {/* TODO: show details of user: roles, registration key, ... (maybe open a modal?)*/}
                   </Tooltip>
                   <Tooltip content="Edit user">
-                    <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                      edit
-                    </span>
+                    <IconPencil className="text-lg cursor-pointer active:opacity-50"></IconPencil>
+                    {/* TODO: make user editable (maybe open a modal or edit in table?) */}
                   </Tooltip>
                   <Tooltip color="danger" content="Delete user">
-                    <span className="text-lg text-danger cursor-pointer active:opacity-50">
-                      delete
-                    </span>
+                    <IconTrash className="text-lg text-danger cursor-pointer active:opacity-50"></IconTrash>
+                    {/* TODO: confirm deletion using popup, then send delete request */}
                   </Tooltip>
                 </div>
               </TableCell>
