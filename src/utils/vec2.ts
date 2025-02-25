0e2f5162a0e1c2059bbe7048b6bdf7ddd6ec8938
fwcd <fwcd@fwcd.dev>
2025-02-26 00:32:30 +0100

    Use consistent naming convention for vec operations

--- src/utils/vec2.ts
+++ src/utils/vec2.ts
@@ -3,33 +3,33 @@ export interface Vec2<T> {
   y: T;
 }
 
-export function add(lhs: Vec2<number>, rhs: Vec2<number>): Vec2<number> {
+export function vec2Add(lhs: Vec2<number>, rhs: Vec2<number>): Vec2<number> {
   return {
     x: lhs.x + rhs.x,
     y: lhs.y + rhs.y,
   };
 }
 
-export function sub(lhs: Vec2<number>, rhs: Vec2<number>): Vec2<number> {
+export function vec2Sub(lhs: Vec2<number>, rhs: Vec2<number>): Vec2<number> {
   return {
     x: lhs.x - rhs.x,
     y: lhs.y - rhs.y,
   };
 }
 
-export function scale(lhs: Vec2<number>, rhs: number): Vec2<number> {
+export function vec2Scale(lhs: Vec2<number>, rhs: number): Vec2<number> {
   return {
     x: lhs.x * rhs,
     y: lhs.y * rhs,
   };
 }
 
-export function length(operand: Vec2<number>): number {
+export function vec2Length(operand: Vec2<number>): number {
   return Math.sqrt(operand.x * operand.x + operand.y * operand.y);
 }
 
-export function dist(lhs: Vec2<number>, rhs: Vec2<number>): number {
-  return length(sub(lhs, rhs));
+export function vec2Dist(lhs: Vec2<number>, rhs: Vec2<number>): number {
+  return vec2Length(vec2Sub(lhs, rhs));
 }
 
 export function vec2Equal(lhs: Vec2<number>, rhs: Vec2<number>): boolean {
