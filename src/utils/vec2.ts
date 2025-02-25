75777b9d8fefd0117f34831a654cb21c00bd69bc
fwcd <fwcd@fwcd.dev>
2025-02-25 23:25:11 +0100

    Add Vec2<T>

---
+++ src/utils/vec2.ts
@@ -0,0 +1,33 @@
+export interface Vec2<T> {
+  x: T;
+  y: T;
+}
+
+export function add(lhs: Vec2<number>, rhs: Vec2<number>): Vec2<number> {
+  return {
+    x: lhs.x + rhs.x,
+    y: lhs.y + rhs.y,
+  };
+}
+
+export function sub(lhs: Vec2<number>, rhs: Vec2<number>): Vec2<number> {
+  return {
+    x: lhs.x - rhs.x,
+    y: lhs.y - rhs.y,
+  };
+}
+
+export function scale(lhs: Vec2<number>, rhs: number): Vec2<number> {
+  return {
+    x: lhs.x * rhs,
+    y: lhs.y * rhs,
+  };
+}
+
+export function length(operand: Vec2<number>): number {
+  return Math.sqrt(operand.x * operand.x + operand.y * operand.y);
+}
+
+export function dist(lhs: Vec2<number>, rhs: Vec2<number>): number {
+  return length(sub(lhs, rhs));
+}
