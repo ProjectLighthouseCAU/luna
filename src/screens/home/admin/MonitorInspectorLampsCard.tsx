c0e42e9d2069a381150e6d63484bfb648f87cc08
fwcd <fwcd@fwcd.dev>
2025-02-26 03:21:37 +0100

    Remove commented out code

--- src/screens/home/admin/MonitorInspectorLampsCard.tsx
+++ src/screens/home/admin/MonitorInspectorLampsCard.tsx
@@ -28,47 +28,6 @@ export function MonitorInspectorLampsCard({
         names={names}
         render={value => <MonitorInspectorValue value={value} />}
       />
-      {/* <div className="flex flex-row">
-        {metrics
-          ? metrics.map((lamp: any, idx: number) => (
-              <div key={idx}>
-                <b>Lamp {idx + 1}:</b>
-                <div>
-                  Responding:{' '}
-                  {lamp.responding ? (
-                    <Chip color="success" variant="flat">
-                      true
-                    </Chip>
-                  ) : (
-                    <Chip color="danger" variant="flat">
-                      false
-                    </Chip>
-                  )}
-                </div>
-                <div>Firmware-Version: {lamp.firmware_version}</div>
-                <div>Uptime (not very accurate): {lamp.uptime}s</div>
-                <div>Timeout: {lamp.timeout}s</div>
-                <div>
-                  Temperature (not very accurate): {lamp.temperature}
-                  °C
-                </div>
-                <div>
-                  Fuse tripped?{' '}
-                  {lamp.fuse_tripped ? (
-                    <Chip color="danger" variant="flat">
-                      Yes
-                    </Chip>
-                  ) : (
-                    <Chip color="success" variant="flat">
-                      No
-                    </Chip>
-                  )}
-                </div>
-                <div>Flashing status: {lamp.flashing_status}</div>
-              </div>
-            ))
-          : undefined}
-      </div> */}
     </TitledCard>
   );
 }
