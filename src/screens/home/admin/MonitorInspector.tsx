284fa5572889a81879b5a662f37e8539b52158a5
fwcd <fwcd@fwcd.dev>
2025-02-26 02:09:27 +0100

    Use key properly in monitor inspector lamp metrics

--- src/screens/home/admin/MonitorInspector.tsx
+++ src/screens/home/admin/MonitorInspector.tsx
@@ -84,44 +84,42 @@ export function MonitorInspector({ metrics }: MonitorInspectorProps) {
             </CardHeader>
             <CardBody>
               {metrics.lamp_metrics.map((lamp: any, idx: number) => (
-                <>
+                <div key={idx}>
+                  <br />
+                  <b>Lamp {idx + 1}:</b>
                   <div>
-                    <br />
-                    <b>Lamp {idx + 1}:</b>
-                    <div>
-                      Responding:{' '}
-                      {lamp.responding ? (
-                        <Chip color="success" variant="flat">
-                          true
-                        </Chip>
-                      ) : (
-                        <Chip color="danger" variant="flat">
-                          false
-                        </Chip>
-                      )}
-                    </div>
-                    <div>Firmware-Version: {lamp.firmware_version}</div>
-                    <div>Uptime (not very accurate): {lamp.uptime}s</div>
-                    <div>Timeout: {lamp.timeout}s</div>
-                    <div>
-                      Temperature (not very accurate): {lamp.temperature}
-                      °C
-                    </div>
-                    <div>
-                      Fuse tripped?{' '}
-                      {lamp.fuse_tripped ? (
-                        <Chip color="danger" variant="flat">
-                          Yes
-                        </Chip>
-                      ) : (
-                        <Chip color="success" variant="flat">
-                          No
-                        </Chip>
-                      )}
-                    </div>
-                    <div>Flashing status: {lamp.flashing_status}</div>
+                    Responding:{' '}
+                    {lamp.responding ? (
+                      <Chip color="success" variant="flat">
+                        true
+                      </Chip>
+                    ) : (
+                      <Chip color="danger" variant="flat">
+                        false
+                      </Chip>
+                    )}
                   </div>
-                </>
+                  <div>Firmware-Version: {lamp.firmware_version}</div>
+                  <div>Uptime (not very accurate): {lamp.uptime}s</div>
+                  <div>Timeout: {lamp.timeout}s</div>
+                  <div>
+                    Temperature (not very accurate): {lamp.temperature}
+                    °C
+                  </div>
+                  <div>
+                    Fuse tripped?{' '}
+                    {lamp.fuse_tripped ? (
+                      <Chip color="danger" variant="flat">
+                        Yes
+                      </Chip>
+                    ) : (
+                      <Chip color="success" variant="flat">
+                        No
+                      </Chip>
+                    )}
+                  </div>
+                  <div>Flashing status: {lamp.flashing_status}</div>
+                </div>
               ))}
             </CardBody>
           </>
