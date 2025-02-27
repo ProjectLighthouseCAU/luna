834d05894f8a07cda082997fe9de08cb010f0b69
fwcd <fwcd@fwcd.dev>
2025-02-27 19:36:17 +0100

    Fill vertical space in resources view

--- src/screens/home/admin/ResourcesTreeView.tsx
+++ src/screens/home/admin/ResourcesTreeView.tsx
@@ -38,8 +38,8 @@ export function ResourcesTreeView({
   switch (layout) {
     case 'column':
       return (
-        <div className="flex flex-row gap-2">
-          <div className="flex flex-col gap-2">
+        <div className="flex flex-row gap-2 h-full">
+          <div className="flex flex-col gap-2 h-full">
             {sortedEntries
               ? sortedEntries.map(([name, subTree]) => (
                   <ResourcesTreeButton
