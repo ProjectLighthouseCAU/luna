476d834d62d27aa8168e0677d19822e7d5ea62d7
fwcd <fwcd@fwcd.dev>
2025-02-27 04:59:11 +0100

    Add mapResult

--- src/utils/result.ts
+++ src/utils/result.ts
@@ -10,6 +10,17 @@ export function errorResult<T, E>(error: E): Result<T, E> {
   return { ok: false, error };
 }
 
+export function mapResult<T, E, U>(
+  result: Result<T, E>,
+  transform: (value: T) => U
+): Result<U, E> {
+  if (result.ok) {
+    return { ok: true, value: transform(result.value) };
+  } else {
+    return result;
+  }
+}
+
 export function getOrThrow<T, E>(result: Result<T, E>): T {
   if (result.ok) {
     return result.value;
