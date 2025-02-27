d2bb7ddcc3c32ae9b9d6e4ce6625ae0ba7fc5d55
fwcd <fwcd@fwcd.dev>
2025-02-27 22:11:53 +0100

    Implement better synchronization in contents view

--- src/screens/home/admin/ResourcesContentsView.tsx
+++ src/screens/home/admin/ResourcesContentsView.tsx
@@ -1,6 +1,6 @@
 import { Spinner } from '@heroui/react';
 import { ModelContext } from '@luna/contexts/api/model/ModelContext';
-import { useContext, useEffect, useState } from 'react';
+import { useCallback, useContext, useEffect, useRef, useState } from 'react';
 
 export interface ResourcesContentsViewProps {
   path: string[];
@@ -8,24 +8,54 @@ export interface ResourcesContentsViewProps {
 
 export function ResourcesContentsView({ path }: ResourcesContentsViewProps) {
   const model = useContext(ModelContext);
-  const [value, setValue] = useState<any>();
+
+  const [valueWrapper, setValue] = useState<{
+    value: any;
+    userEdited: boolean;
+  }>({ value: undefined, userEdited: false });
+
   const [error, setError] = useState<string>();
+  const preRef = useRef<HTMLPreElement | null>(null);
 
   useEffect(() => {
     (async () => {
       const result = await model.get(path);
       if (result.ok) {
-        setValue(result.value);
+        setValue({ value: result.value, userEdited: false });
       } else {
         setError(`${result.error}`);
       }
     })();
   }, [model, path]);
 
+  const onChange = useCallback(() => {
+    const pre = preRef.current;
+    if (pre === null) {
+      return;
+    }
+    try {
+      const parsedValue = JSON.parse(pre.innerText);
+      console.log('Parsed', parsedValue);
+      setValue({ value: parsedValue, userEdited: true });
+    } catch {
+      // Swallow parse errors
+    }
+  }, []);
+
+  useEffect(() => {
+    const pre = preRef.current;
+    if (pre === null) {
+      return;
+    }
+    if (!valueWrapper.userEdited) {
+      pre.innerText = JSON.stringify(valueWrapper.value, null, 2);
+    }
+  }, [valueWrapper]);
+
   return (
     <div>
-      {value !== undefined ? (
-        <pre contentEditable>{JSON.stringify(value, null, 2)}</pre>
+      {valueWrapper !== undefined ? (
+        <pre ref={preRef} contentEditable onInput={onChange} />
       ) : error ? (
         error
       ) : (
