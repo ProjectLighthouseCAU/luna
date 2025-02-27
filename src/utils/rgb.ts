884d6876fb34ff1ccb1bfd9bc5258c92677ea067
fwcd <fwcd@fwcd.dev>
2025-02-27 03:07:38 +0100

    Shorten function names in favor of qualified imports

--- src/utils/rgb.ts
+++ src/utils/rgb.ts
@@ -1,37 +1,37 @@
 /// An RGB color represented as a 32-bit integer.
-export type RGBColor = number;
+export type Color = number;
 
-export const BLACK: RGBColor = 0x000000;
-export const WHITE: RGBColor = 0xffffff;
-export const RED: RGBColor = 0xff0000;
-export const GREEN: RGBColor = 0x00ff00;
-export const BLUE: RGBColor = 0x0000ff;
-export const YELLOW: RGBColor = 0xffff00;
-export const CYAN: RGBColor = 0x00ffff;
-export const MAGENTA: RGBColor = 0xff00ff;
+export const BLACK: Color = 0x000000;
+export const WHITE: Color = 0xffffff;
+export const RED: Color = 0xff0000;
+export const GREEN: Color = 0x00ff00;
+export const BLUE: Color = 0x0000ff;
+export const YELLOW: Color = 0xffff00;
+export const CYAN: Color = 0x00ffff;
+export const MAGENTA: Color = 0xff00ff;
 
 /// Creates an RGB color from the given red, green and blue components (as bytes between 0-255).
-export function rgbColor(red: number, green: number, blue: number): number {
+export function color(red: number, green: number, blue: number): number {
   return (red << 16) | (green << 8) | blue;
 }
 
 /// Fetches the red component (a byte in the range 0-255).
-export function getRed(color: RGBColor): number {
+export function getRed(color: Color): number {
   return (color >> 16) & 0xff;
 }
 
 /// Fetches the green component (a byte in the range 0-255).
-export function getGreen(color: RGBColor): number {
+export function getGreen(color: Color): number {
   return (color >> 8) & 0xff;
 }
 
 /// Fetches the blue component (a byte in the range 0-255).
-export function getBlue(color: RGBColor): number {
+export function getBlue(color: Color): number {
   return color & 0xff;
 }
 
 /// Fetches the `i`th color in the given (interleaved) RGB buffer.
-export function getRGBColorAt(i: number, rgbBuffer: Uint8Array): RGBColor {
+export function getAt(i: number, rgbBuffer: Uint8Array): Color {
   return (
     (rgbBuffer[i * 3] << 16) |
     (rgbBuffer[i * 3 + 1] << 8) |
@@ -40,19 +40,15 @@ export function getRGBColorAt(i: number, rgbBuffer: Uint8Array): RGBColor {
 }
 
 /// Sets the `i`th color to the given value in the given (interleaved) RGB buffer.
-export function setRGBColorAt(
-  i: number,
-  color: RGBColor,
-  rgbBuffer: Uint8Array
-): void {
+export function setAt(i: number, color: Color, rgbBuffer: Uint8Array): void {
   rgbBuffer[i * 3] = (color >> 16) & 0xff;
   rgbBuffer[i * 3 + 1] = (color >> 8) & 0xff;
   rgbBuffer[i * 3 + 2] = color & 0xff;
 }
 
 /// Linearly interpolates between two colors.
-export function lerp(lhs: RGBColor, rhs: RGBColor, value: number): RGBColor {
-  return rgbColor(
+export function lerp(lhs: Color, rhs: Color, value: number): Color {
+  return color(
     (1 - value) * getRed(lhs) + value * getRed(rhs),
     (1 - value) * getGreen(lhs) + value * getGreen(rhs),
     (1 - value) * getBlue(lhs) + value * getBlue(rhs)
