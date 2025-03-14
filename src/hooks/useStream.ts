9f795243b5386e123e00a1e6deba0879d733ed87
fwcd <fwcd@fwcd.dev>
2025-03-14 01:14:59 +0100

    Fix re-render loops in useStream with a new useJsonMemo hook

--- src/hooks/useStream.ts
+++ src/hooks/useStream.ts
@@ -1,8 +1,14 @@
 import { ModelContext } from '@luna/contexts/api/model/ModelContext';
 import { useAsyncIterable } from '@luna/hooks/useAsyncIterable';
+import { useJsonMemo } from '@luna/hooks/useJsonMemo';
 import { useCallback, useContext, useState } from 'react';
 
-export function useStream<T>(path: string[]): T | undefined {
+export function useStream<T>(path_: string[]): T | undefined {
+  // Prevent re-render loops if the passed path is a literal by rountripping it
+  // through JSON (and thus a different instance each time, thus stopping +
+  // starting a new stream on each render).
+  const path = useJsonMemo(path_);
+
   const { api } = useContext(ModelContext);
   const [value, setValue] = useState<T>();
 
