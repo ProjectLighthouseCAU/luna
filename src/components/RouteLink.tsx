c8712e62c76a6cb969f1f66e6d862bdb52f36ec8
fwcd <fwcd@fwcd.dev>
2025-03-17 21:01:37 +0100

    Reimplement sidebar chevron using custom div

--- src/components/RouteLink.tsx
+++ src/components/RouteLink.tsx
@@ -1,9 +1,16 @@
-import { Button } from '@heroui/react';
+import { Button, PressEvent } from '@heroui/react';
 import { IconChevronDown, IconChevronRight } from '@tabler/icons-react';
-import { ReactNode, useCallback, useState } from 'react';
+import {
+  MouseEventHandler,
+  ReactNode,
+  useCallback,
+  useContext,
+  useState,
+} from 'react';
 import { NavLink } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { AnimatePresence } from '@luna/utils/motion';
+import { ColorSchemeContext } from '@luna/contexts/env/ColorSchemeContext';
 
 interface RouteLinkParams {
   icon: ReactNode;
@@ -21,9 +28,11 @@ export function RouteLink({
   children,
 }: RouteLinkParams) {
   const [isExpanded, setExpanded] = useState(true);
+  const { colorScheme } = useContext(ColorSchemeContext);
 
-  const toggleExpanded = useCallback(() => {
+  const onPressChevron = useCallback<MouseEventHandler<HTMLDivElement>>(e => {
     setExpanded(isExpanded => !isExpanded);
+    e.preventDefault();
   }, []);
 
   return isSkeleton ? (
@@ -39,21 +48,24 @@ export function RouteLink({
           } px-2 py-1.5 rounded flex flex-row items-center justify-between h-10`
         }
       >
-        <div className="flex flex-row gap-2">
-          {icon}
-          {name}
-        </div>
-        {children ? (
-          <Button
-            isIconOnly
-            onPress={toggleExpanded}
-            variant="light"
-            size="sm"
-            className="text-inherit"
-          >
-            {isExpanded ? <IconChevronDown /> : <IconChevronRight />}
-          </Button>
-        ) : null}
+        {({ isActive }) => (
+          <>
+            <div className="flex flex-row gap-2">
+              {icon}
+              {name}
+            </div>
+            {children ? (
+              // Intentionally not using a button for the chevron to avoid
+              // https://github.com/ProjectLighthouseCAU/luna/issues/45
+              <div
+                className={`p-0.5 rounded-md ${isActive || colorScheme.isDark ? 'hover:bg-[rgba(255,255,255,0.2)]' : 'hover:bg-[rgba(128,128,128,0.2)]'} active:opacity-80`}
+                onClick={onPressChevron}
+              >
+                {isExpanded ? <IconChevronDown /> : <IconChevronRight />}
+              </div>
+            ) : null}
+          </>
+        )}
       </NavLink>
       <AnimatePresence initial={isExpanded}>
         {isExpanded ? (
