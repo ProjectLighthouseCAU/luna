2dc7142f9526dad1caef3f52eb89798f42491661
fwcd <fwcd@fwcd.dev>
2025-03-19 04:04:02 +0100

    Check that user actually exists

--- src/hooks/useLiveUser.ts
+++ src/hooks/useLiveUser.ts
@@ -1,18 +1,21 @@
+import { ModelContext } from '@luna/contexts/api/model/ModelContext';
 import { useStream } from '@luna/hooks/useStream';
-import { useMemo } from 'react';
+import { useContext, useMemo } from 'react';
 
 export function useLiveUser() {
   const livePath = useStream<string[]>(['live']);
+  const { users } = useContext(ModelContext);
 
   const liveUsername = useMemo(
     () =>
       livePath &&
       livePath.length === 3 &&
       livePath[0] === 'user' &&
-      livePath[2] === 'model'
+      livePath[2] === 'model' &&
+      users.all.contains(livePath[1])
         ? livePath[1]
         : undefined,
-    [livePath]
+    [livePath, users.all]
   );
 
   return { liveUsername };
