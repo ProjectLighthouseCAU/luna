dbfd179ea69fc16c1d2ecc7b61739ca612218212
fwcd <fwcd@fwcd.dev>
2025-03-22 21:55:14 +0100

    Recursively merge values in useLocalStorage
    This allows for more sophisticated 'repairs' of old data.

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,6 +1,26 @@
 import { LocalStorageKey } from '@luna/constants/LocalStorageKey';
 import { useEffect, useMemo, useState } from 'react';
 
+/** Recursively merges two values, discarding anything not matching `base`'s schema. */
+function merge(base: any, delta: any): any {
+  if (typeof base === typeof delta) {
+    // TODO: This doesn't deal with schema changes in arrays, where stale values
+    // from the delta may still be kept. Is this a case we need to consider?
+    if (typeof base === 'object' && !Array.isArray(base)) {
+      const merged = { ...base };
+      for (const key in base) {
+        if (key in delta) {
+          merged[key] = merge(base[key], delta[key]);
+        }
+      }
+      return merged;
+    } else {
+      return delta;
+    }
+  }
+  return base;
+}
+
 export function useLocalStorage<T>(
   key: LocalStorageKey,
   defaultValue: () => T
@@ -10,21 +30,7 @@ export function useLocalStorage<T>(
     let value = defaultValue();
     if (raw) {
       const parsed = JSON.parse(raw);
-      if (typeof value === typeof parsed) {
-        if (typeof value === 'object') {
-          // Merge objects by only considering known keys
-          for (const key in value) {
-            if (key in parsed) {
-              value[key] = parsed[key];
-            }
-          }
-        } else {
-          value = parsed;
-        }
-      } else {
-        // We stored something else and will just keep the default
-        // This might happen if the frontend updated this to a different type
-      }
+      value = merge(value, parsed);
     }
     return value;
   });
