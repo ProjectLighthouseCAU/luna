ede538646b3b5100de0194abda1342f816bb20c8
fwcd <fwcd@fwcd.dev>
2025-03-25 22:36:11 +0100

    Implement animator scrubbing

--- src/components/AnimatorActionSnippet.tsx
+++ src/components/AnimatorActionSnippet.tsx
@@ -1,29 +1,62 @@
-import { Progress } from '@heroui/react';
+import { Slider } from '@heroui/react';
 import { ColorSnippet } from '@luna/components/ColorSnippet';
 import { AnimatorAction } from '@luna/contexts/displays/animator/types';
+import { useCallback, useState } from 'react';
 
 export interface AnimatorActionSnippetProps {
   action: AnimatorAction;
   displayProgress?: boolean;
+  scrubProgress?: (ticks: number) => void;
+  isPlaying: boolean;
+  setPlaying?: (isPlaying: boolean) => void;
   className?: string;
 }
 
 export function AnimatorActionSnippet({
   action,
   displayProgress = false,
+  scrubProgress,
+  isPlaying,
+  setPlaying,
   className = '',
 }: AnimatorActionSnippetProps) {
+  const [isScrubbing, setScrubbing] = useState(false);
+  const [wasPlaying, setWasPlaying] = useState<boolean>();
+
+  const onChangeProgress = useCallback(
+    (value: number | number[]) => {
+      if (typeof value === 'number' && scrubProgress) {
+        if (!isScrubbing) {
+          setScrubbing(true);
+          setWasPlaying(isPlaying);
+          setPlaying?.(false);
+        }
+        scrubProgress(value);
+      }
+    },
+    [isPlaying, isScrubbing, scrubProgress, setPlaying]
+  );
+
+  const onChangeEndProgress = useCallback(() => {
+    if (isScrubbing && wasPlaying !== undefined) {
+      setPlaying?.(wasPlaying);
+      setWasPlaying(undefined);
+    }
+  }, [isScrubbing, setPlaying, wasPlaying]);
+
   return (
     <div className={`flex flex-col ${className}`}>
       <div>{formatType(action.type)}</div>
       <AnimatorActionDetail action={action} />
       {displayProgress ? (
-        <Progress
+        <Slider
           aria-label="Animator action progress"
           size="sm"
           value={action.ticks.value}
           maxValue={action.ticks.total - 1}
-          color="default"
+          onChange={onChangeProgress}
+          onChangeEnd={onChangeEndProgress}
+          color="foreground"
           className="mt-1"
         />
       ) : null}
